f51e29ac1020ac064fe163791db4d0021419dfb9
CKDOutcast <morabet.amin@gmail.com>
2024-05-19 08:14:20 +0100

    adjusting more

--- src/components/AuthButtons.jsx
+++ src/components/AuthButtons.jsx
@@ -1,6 +1,7 @@
 import React, { useState} from 'react'
 import { auth } from "../firebase/config"
 import { signOut} from  "firebase/auth"
+import { HashLink } from 'react-router-hash-link';
 import { useNavigate } from 'react-router-dom'
 import Button from './Button'
 import { toast } from 'react-toastify';
@@ -38,24 +39,24 @@ const navigate = useNavigate();
       {isLoading && <Loader />}
       {authenticatedUser === null ? (
         <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
-          <a
+          <HashLink
             href="/Register"
             className="button hidden mr-8 text-n-8/80 transition-colors hover:text-pink-600/90 lg:block font-grotesk"
           >
             New Account
-          </a>
-          <Button className="hidden lg:flex font-grotesk" href="/login">
+          </HashLink>
+          <Button className="hidden lg:flex font-grotesk" href="/react-tensorflow-website/#/login">
             Sign in
           </Button>
         </div>
       ) : (
         <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row font-grotesk">
-          <a
+          <HashLink
             href="/Classifier"
             className="button hidden mr-8 text-n-8/80 transition-colors hover:text-violet-500/90 hover:bg-n-1 lg:block border-2 border-[#ffffcc] px-4 py-3 rounded-2xl"
           >
             ZooLens App
-          </a>
+          </HashLink>
           <Button className="hidden lg:flex font-grotesk" onClick={logOut}>
             Log out
           </Button>
